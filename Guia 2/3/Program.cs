fc753d1f8cd0b6c7f7bac4779474e7cf9318d9a8
Ezequiel Lorenz <eze_lorenz@hotmail.com>
2022-07-13 08:23:51 -0300

    Ejercicios 2-3-4 Guia 2

---
+++ Guia 2/3/Program.cs
@@ -0,0 +1,22 @@
+﻿class Program
+{
+
+    /*
+    Relaizar un programa que pida un numero y determine si es par o impar
+    mostrar en pantalla si es par o impar.
+    */
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Ingrese un numero entero");
+        int numero = Int32.Parse(Console.ReadLine());
+
+        if (numero % 2 == 0)
+        {
+            Console.WriteLine("El numero ingraseado es par");
+        }
+        else
+        {
+            Console.WriteLine("El numero ingresado es impar");
+        }
+    }
+}
